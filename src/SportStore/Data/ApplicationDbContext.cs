ebec97c0d167e92eafeb793c1e615c4546244598
nvanderperren <ik.ben.nas@gmail.com>
2017-01-15 19:31:36 +0100

    Add class City and table City

--- src/SportStore/Data/ApplicationDbContext.cs
+++ src/SportStore/Data/ApplicationDbContext.cs
@@ -7,6 +7,8 @@ namespace SportStore.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<City> Cities { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionstring = @"Server=.\SQLEXPRESS;Database=SportStore;Integrated Security=True";
@@ -16,6 +18,17 @@ namespace SportStore.Data
         protected override void OnModelCreating(ModelBuilder modelbuiler)
         {
             modelbuiler.Entity<Product>(MapProduct);
+            modelbuiler.Entity<City>(MapCity);
+        }
+
+        private void MapCity(EntityTypeBuilder<City> c)
+        {
+            c.ToTable("City");
+
+            c.HasKey(t => t.Postalcode);
+
+            c.Property(t => t.Name)
+                .HasMaxLength(100);
         }
 
         private static void MapProduct(EntityTypeBuilder<Product> p)
---
+++ src/SportStore/Migrations/20170115182914_CreateTableCity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportStore.Migrations
+{
+    public partial class CreateTableCity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Postalcode = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Postalcode);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
