1080d789384815a08eb53406351371fa43e2b522
nvanderperren <ik.ben.nas@gmail.com>
2017-01-15 14:50:23 +0100

    add ApplicationDbContext and configuration database

---
+++ src/SportStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring = @"Server=.\SQLEXPRESS;Database=SportStore;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+    }
+}
